8f83e8b19155b2773806fcf98f0416954da1d501
somayya <somayya.adusumalli98@gmail.com>
2025-09-29 14:52:12 +0530

    session-06

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ $G_ID="sg-04f9b24ffc7783a86"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids 
     $G_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances
     [0].InstanceId' --output text)
    
