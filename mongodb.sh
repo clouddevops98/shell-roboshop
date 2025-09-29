2e0099d1a11edbe193d001ddfed86848fb4bc196
somayya <somayya.adusumalli98@gmail.com>
2025-09-29 16:59:58 +0530

    session-06

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[34m"
 
-LOGS_FOLDER="/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
