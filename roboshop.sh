cd25d679fc5eae69984a15560a65b0da9a245489
somayya <somayya.adusumalli98@gmail.com>
2025-09-29 14:27:28 +0530

    session-06

--- roboshop.sh
+++ roboshop.sh
@@ -5,16 +5,16 @@ $G_ID="sg-04f9b24ffc7783a86"
 
 for instance in $@
 do
-   aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids 
-   sg-04f9b24ffc7783a86 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,
-   Value=$instance}]" --query 'Instances[0].InstanceId' --output text
+   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids 
+   $G_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,
+   Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
    
    #Get Private IP
    if [ $instance != "frontend" ]; then
-       IP=$(aws ec2 describe-instances --instance-ids i-0bd78c4903e53c4bb --query 'Reservations[0].Instances
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances
        [0].PublicIpAddress' --output text)
     else
-       IP=$(aws ec2 describe-instances --instance-ids i-0bd78c4903e53c4bb --query 'Reservations[0].Instances
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances
        [0].PrivateIpAddress' --output text)
    fi
 
