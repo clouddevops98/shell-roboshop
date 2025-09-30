2747331149aa96442e29280ff445f02cfe809609
somayya <somayya.adusumalli98@gmail.com>
2025-09-30 15:31:33 +0530

    session-06

--- catalogue.sh
+++ catalogue.sh
@@ -45,7 +45,7 @@ if [ $? -ne 0 ]; then
 else
      echo  -e "User already exist ... $Y SKIPPING $N"
 fi
-mkdir /app 
+mkdir  -p /app 
 VALIDATE $? "Creating app directory"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading catalogue application"
