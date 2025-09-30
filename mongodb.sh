08ace550b3e8f465b1f38afb31508d3609f42283
somayya <somayya.adusumalli98@gmail.com>
2025-09-30 11:47:22 +0530

    session-06

--- mongodb.sh
+++ mongodb.sh
@@ -39,4 +39,8 @@ VALIDATE $? "Enable MongoDB"
 systemctl start mongod 
 VALIDATE $? "Start MongoDB"
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to mongoDB"
 
+systemctl restart mongodb
+VALIDATE $? "Restarted MongoDB"
