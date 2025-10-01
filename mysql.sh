ba1b38079ede05ce8922f449ab62659452092b7b
somayya <somayya.adusumalli98@gmail.com>
2025-10-01 17:23:04 +0530

    session-06

--- mysql.sh
+++ mysql.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-
+START_TIME=$(date +%s)
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
@@ -38,3 +38,7 @@ VALIDATE $? "Starting MYSQL Server"
 
 mysql_secure_installation --set-root-pass RoboShop@1
 VALIDATE $? "Setting up Root password"
+
+END_TIME=$(date +%s)
+Total_time=$(($END_TIME - $START_TIME))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
